aaa15e5242239e02cc1743162ad4941cb4a8b505
NyatiZw <appsmaid19@gmail.com>
2023-04-24 14:29:41 -0700

    working on selection sort comments

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,4 +1,9 @@
 #include "sort.h"
+/**
+ * swapFunc:- 'Function to swap integers'
+ * @a: integer value
+ * @b: integer value
+ */
 
 void swapFunc(int *a, int *b)
 {
@@ -7,13 +12,23 @@ void swapFunc(int *a, int *b)
 	*b = temp;
 }
 
+/**
+ * selection_sort:- 'Function to arrange integers in ascending order'
+ * @array: Array of integers
+ * @size: Size of array
+ */
 
 void selection_sort(int *array, size_t size)
 {
 	size_t i, step;
 	int min_idx;
 
-	for (step = 0; step < size -1; step++)
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+
+	for (step = 0; step < size - 1; step++)
 	{
 		min_idx = step;
 		for (i = step + 1; i < size; i++)
