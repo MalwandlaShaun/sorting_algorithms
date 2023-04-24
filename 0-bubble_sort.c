ef1b913b1c4d07233ac57374df63c765f0e2b51a
NyatiZw <appsmaid19@gmail.com>
2023-04-24 08:33:12 -0700

    betty checking bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 /**
  * SwapFunc:- Function to swap int
- * 
+ *
  * @i: integer value
  * @j: integer value
  */
@@ -27,13 +27,13 @@ void bubble_sort(int *array, size_t size)
 	size_t x;
 	size_t y;
 
-	for(x = 0; x < size-1; ++x)
+	for (x = 0; x < size - 1; ++x)
 	{
-		for(y = 0; y < size -x -1; ++y)
+		for (y = 0; y < size - x - 1; ++y)
 		{
-			if (array[y] > array[y+1])
+			if (array[y] > array[y + 1])
 			{
-				SwapFunc(&array[y], &array[y+1]);
+				SwapFunc(&array[y], &array[y + 1]);
 				print_array(array, size);
 			}
 		}
