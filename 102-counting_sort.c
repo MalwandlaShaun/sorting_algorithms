9ea0a2ef37686f11edf70f2215960fe023bf545c
NyatiZw <appsmaid19@gmail.com>
2023-04-27 10:39:28 -0700

    counting sort task

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,4 +1,26 @@
 #include "sort.h"
+/**
+ * max_element:- Get maximum element in array
+ * @array: An array of integers
+ * @size: size of the array
+ *
+ * Return: The max integer
+ */
+int get_max_element(int *array, int size)
+{
+	int maximum_element, i;
+
+	for (maximum_element = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > maximum_element)
+		{
+			maximum_element = array[i];
+		}
+	}
+
+	return (maximum_element);
+}
+
 
 /**
  * counting_sort:- Sort array using the counting sort
@@ -8,57 +30,55 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	int count_size;
-	int max, min;
-	size_t i;
+	int max;
+	int i;
 	int *count_array;
 	int *output_array;
 
-	max = array[0], min = array[0];
-
-	for (i = 0; i < size; i++)
+	if (array == NULL || size < 2)
 	{
-		if (array[i] > max)
-		{
-			max = array[i];
-		}
-		if (array[i] < min)
-		{
-			min = array[i];
-		}
+		return;
 	}
 
-	count_size = max - min + 1;
-	count_array = (int *) malloc(count_size * sizeof(int));
-	for (i = 0; i < (size_t)count_size; i++)
+	output_array = malloc(sizeof(int) * size);
+	if (output_array == NULL)
+	{
+		return;
+	}
+	max = get_max_element(array, size);
+	count_array = malloc(sizeof(int) * (max + 1));
+	if (count_array == NULL)
+	{
+		free(output_array);
+		return;
+	}
+
+	for (i = 0; i < (max + 1); i++)
 	{
 		count_array[i] = 0;
 	}
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < (int)size; i++)
 	{
-		count_array[array[i] - min]++;
+		count_array[array[i]] += 1;
 	}
 
-	for (i = 1; i < (size_t)count_size; i++)
+	for (i = 0; i < (max + 1); i++)
 	{
 		count_array[i] += count_array[i - 1];
 	}
+	print_array(count_array, max + 1);
 
-	output_array = (int *) malloc(size * sizeof(int));
-
-	for (i = 0; i < size; i++)
+	for (i = 0; i < (int)size; i++)
 	{
-		output_array[count_array[array[i] - min] - 1] = array[i];
-		count_array[array[i] - min]--;
+		output_array[count_array[array[i]] -1] = array[i];
+		count_array[array[i]] -= 1;
 	}
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < (int)size; i++)
 	{
 		array[i] = output_array[i];
 	}
-
-	print_array(count_array, count_size);
 
 	free(count_array);
 	free(output_array);
