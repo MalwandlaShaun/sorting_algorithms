ddbfed815bd3ae183738a6df6c70b6394c9b63eb
NyatiZw <appsmaid19@gmail.com>
2023-04-28 05:32:02 -0700

    checking last file

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,36 +1,50 @@
 #include "sort.h"
 
-void bitonic_sort(int *array, size_t size)
+void bitonic_sort(int * array, size_t size)
 {
-	int i, j, k;
-	int prod, temp;
+	int *temp;
 
-	for (k = 2; k <= (int)size; k = k* 2)
+	temp = malloc(size * sizeof(int));
+	if (temp == NULL)
 	{
-		/* Build bitonic sequence */
-		for (j = k / 2; j > 0; j = j / 2)
-		{
-			for (i = 0; i < (int)size; i++)
-			{
-				prod = i ^ j;
-				if (prod > i)
-				{
-					if (( i & k) == 0 && array[i] > array[prod])
-					{
-						temp = array[i];
-						array[i] = array[prod];
-						array[prod] = temp;
-					}
-					if ((i & k) != 0 && array[i] < array[prod])
-					{
-						temp = array[i];
-						array[i] = array[prod];
-						array[prod] = temp;
-					}
-				}
-			}
-		}
+		printf("Error: Memory allocation failed");
+		return;
+	}
+	bitonic_sort_helper(array, 0, size, 1, temp);
+	free(temp);
+}
+
+void bitonic_sort_helper(int *array, size_t start, size_t count, int direction, int *temp)
+{
+	size_t k;
+
+	if (count > 1)
+	{
+		k = count / 2;
+		bitonic_sort_helper(array, start, k, 1, temp);
+		bitonic_sort_helper(array, start + k, k, 0, temp);
+		bitonic_merge(array, start, count, direction, temp);
 	}
 }
 
+void bitonic_merge(int *array, size_t start, size_t count, int direction, int *temp)
+{
+	int temp_val;
+	size_t k, i;
 
+	if (count > 1)
+	{
+		k = count / 2;
+		for (i = start; i < start + k; i++)
+		{
+			if (direction == (array[i] > array[i + k]))
+			{
+				temp_val = array[i];
+				array[i] = array[i + k];
+				array[i + k] = temp_val;
+			}
+		}
+		bitonic_merge(array, start, k, direction, temp);
+		bitonic_merge(array, start + k, k, direction, temp);
+	}
+}
