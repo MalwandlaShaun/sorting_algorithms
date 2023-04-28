d508a9c1d3b06168fa34fdd09898f0123ed10134
NyatiZw <appsmaid19@gmail.com>
2023-04-28 15:39:20 -0700

    checking bitonic sort

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -7,19 +7,27 @@ void bitonic_sort(int *array, size_t size)
 		printf("Error: Memory allocation failed");
 		return;
 	}
-	print_array(array, size);
 	bitonic_sort_helper(array, 0, size, 1, temp);
 	free(temp);
 }
 
 void bitonic_sort_helper(int *array, size_t start, size_t count, int direction, int *temp)
 {
+	size_t i, k;
+
 	if (count > 1)
 	{
-		size_t k = count / 2;
+		printf("Merging [%d/%d] (%d)\n", array[count], array[start], array[direction]);
+		k = count / 2;
 		bitonic_sort_helper(array, start, k, 1, temp);
 		bitonic_sort_helper(array, start + k, k, 0, temp);
 		bitonic_merge(array, start, count, direction, temp);
+		printf("Result [%d/%d] (%d)\n", array[start], array[count], array[direction]);
+		for (i = start; i < start + count; i++)
+		{
+			printf("%d ", array[i]);
+		}
+		printf("\n");
 	}
 }
 
